630ce8a37f9fd9b288a9d913aa55080ab0fe5e34
O_Gyong <ogyong7143@gmail.com>
2023-12-14 13:44:21 +0900

    PwfbPreferencesRepository 데이터 추가

--- app/src/main/java/com/pwfb/common/DataStoreResult.kt
+++ app/src/main/java/com/pwfb/common/DataStoreResult.kt
@@ -2,4 +2,6 @@ package com.pwfb.common
 
 object DataStoreResult {
     const val SET_NAME = "Set Name Success"
+    const val SET_FIRST_INIT = false
+    const val SET_D_DAY = "Set D-Day Success"
 }
--- app/src/main/java/com/pwfb/repository/PwfbPreferencesRepository.kt
+++ app/src/main/java/com/pwfb/repository/PwfbPreferencesRepository.kt
@@ -1,6 +1,7 @@
 package com.pwfb.repository
 
 import android.content.Context
+import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.emptyPreferences
 import androidx.datastore.preferences.core.stringPreferencesKey
@@ -15,7 +16,7 @@ import javax.inject.Inject
 
 class PwfbPreferencesRepository @Inject constructor(private val context: Context) {
     private val nameKey = stringPreferencesKey("name")
-    private val firstInitKey = stringPreferencesKey("firstInit")
+    private val firstInitKey = booleanPreferencesKey("firstInit")
     private val dDayKey = stringPreferencesKey("dDay")
     private val trainingProgramKey = stringPreferencesKey("trainingProgram")
     private val carbohydrateKey = stringPreferencesKey("carbohydrate")
@@ -27,6 +28,10 @@ class PwfbPreferencesRepository @Inject constructor(private val context: Context
     private val creatineKey = stringPreferencesKey("creatine")
     private val dietaryFiberKey = stringPreferencesKey("dietaryFiber")
 
+
+    /**
+     * 이름 설정
+     */
     suspend fun setName(name: String): String {
         context.dataStore.edit {
             it[nameKey] = name
@@ -42,7 +47,229 @@ class PwfbPreferencesRepository @Inject constructor(private val context: Context
                 throw e
             }
         }.map {
-            it[nameKey] ?: "getNameNull"
+            it[nameKey] ?: "NameNull"
         }
     }
+
+    /**
+     * 최초 진입 여부 설정
+     */
+    suspend fun setFistInit(): Boolean {
+        context.dataStore.edit {
+            it[firstInitKey] = false
+        }
+        return DataStoreResult.SET_FIRST_INIT
+    }
+
+    suspend fun getFirstInit(): Flow<Unit>  {
+        return context.dataStore.data.catch { e ->
+            if (e is IOException) {
+                emit(emptyPreferences())
+            } else {
+                throw e
+            }
+        }.map {
+            // firstInitKey 값이 없을 경우 앱 최초 진입으로 판단
+            it[firstInitKey] ?: true
+        }
+    }
+
+    /**
+     * D-Day 설정
+     */
+    suspend fun setDDay(): String {
+        context.dataStore.edit {
+            // todo : DateTime 사용
+        }
+        return DataStoreResult.SET_D_DAY
+    }
+
+    suspend fun getDDay(): Flow<Unit> {
+        return context.dataStore.data.catch { e ->
+            if (e is IOException) {
+                emit(emptyPreferences())
+            } else {
+                throw e
+            }
+        }.map {
+            it[dDayKey] ?: ""
+        }
+    }
+
+    /**
+     * 운동 계획 설정
+     */
+    suspend fun setTrainingProgram(name: String): String {
+        context.dataStore.edit {
+            it[trainingProgramKey] = name
+        }
+        return DataStoreResult.SET_NAME
+    }
+
+    suspend fun getTrainingProgram(): Flow<Unit> {
+        return context.dataStore.data.catch { e ->
+            if (e is IOException) {
+                emit(emptyPreferences())
+            } else {
+                throw e
+            }
+        }.map {
+            it[trainingProgramKey] ?: ""
+        }
+    }
+
+    /**
+     * 탄수화물 설정
+     */
+    suspend fun setCarbohydrate(name: String): String {
+        context.dataStore.edit {
+            it[nameKey] = name
+        }
+        return DataStoreResult.SET_NAME
+    }
+
+    suspend fun getCarbohydrate(): Flow<Unit> {
+        return context.dataStore.data.catch { e ->
+            if (e is IOException) {
+                emit(emptyPreferences())
+            } else {
+                throw e
+            }
+        }.map {
+            it[nameKey] ?: "NameNull"
+        }
+    }
+
+    /**
+     * 단백질 설정
+     */
+    suspend fun setProtein(name: String): String {
+        context.dataStore.edit {
+            it[nameKey] = name
+        }
+        return DataStoreResult.SET_NAME
+    }
+
+    suspend fun getProtein(): Flow<Unit> {
+        return context.dataStore.data.catch { e ->
+            if (e is IOException) {
+                emit(emptyPreferences())
+            } else {
+                throw e
+            }
+        }.map {
+            it[nameKey] ?: "NameNull"
+        }
+    }
+
+    /**
+     * 지방 설정
+     */
+    suspend fun setFat(name: String): String {
+        context.dataStore.edit {
+            it[nameKey] = name
+        }
+        return DataStoreResult.SET_NAME
+    }
+
+    suspend fun getFat(): Flow<Unit> {
+        return context.dataStore.data.catch { e ->
+            if (e is IOException) {
+                emit(emptyPreferences())
+            } else {
+                throw e
+            }
+        }.map {
+            it[nameKey] ?: "NameNull"
+        }
+    }
+
+    /**
+     * 수분 설정
+     */
+    suspend fun setWater(name: String): String {
+        context.dataStore.edit {
+            it[nameKey] = name
+        }
+        return DataStoreResult.SET_NAME
+    }
+
+    suspend fun getWater(): Flow<Unit> {
+        return context.dataStore.data.catch { e ->
+            if (e is IOException) {
+                emit(emptyPreferences())
+            } else {
+                throw e
+            }
+        }.map {
+            it[nameKey] ?: "NameNull"
+        }
+    }
+
+    /**
+     * 나트륨 설정
+     */
+    suspend fun setSodium(name: String): String {
+        context.dataStore.edit {
+            it[nameKey] = name
+        }
+        return DataStoreResult.SET_NAME
+    }
+
+    suspend fun getSodium(): Flow<Unit> {
+        return context.dataStore.data.catch { e ->
+            if (e is IOException) {
+                emit(emptyPreferences())
+            } else {
+                throw e
+            }
+        }.map {
+            it[nameKey] ?: "NameNull"
+        }
+    }
+
+    /**
+     * 칼륨 설정
+     */
+    suspend fun setPotassium(name: String): String {
+        context.dataStore.edit {
+            it[nameKey] = name
+        }
+        return DataStoreResult.SET_NAME
+    }
+
+    suspend fun getPotassium(): Flow<Unit> {
+        return context.dataStore.data.catch { e ->
+            if (e is IOException) {
+                emit(emptyPreferences())
+            } else {
+                throw e
+            }
+        }.map {
+            it[nameKey] ?: "NameNull"
+        }
+    }
+
+    /**
+     * 크레아틴 설정
+     */
+    suspend fun setCreatine(name: String): String {
+        context.dataStore.edit {
+            it[nameKey] = name
+        }
+        return DataStoreResult.SET_NAME
+    }
+
+    suspend fun getCreatine(): Flow<Unit> {
+        return context.dataStore.data.catch { e ->
+            if (e is IOException) {
+                emit(emptyPreferences())
+            } else {
+                throw e
+            }
+        }.map {
+            it[nameKey] ?: "NameNull"
+        }
+    }
+
 }
