53cdc8fc4c0ec9b355a7f8f1c13993dca9f7852b
O_Gyong <ogyong7143@gmail.com>
2023-12-25 19:09:46 +0900

    Nutrition UI 작업

--- app/src/main/java/com/pwfb/ui/nutrition/NutritionFragment.kt
+++ app/src/main/java/com/pwfb/ui/nutrition/NutritionFragment.kt
@@ -1,15 +1,24 @@
 package com.pwfb.ui.nutrition
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.pwfb.base.BaseFragment
 import com.pwfb.databinding.FragmentNutritionBinding
+import com.pwfb.ui.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import java.text.SimpleDateFormat
+import java.util.Calendar
 
+
+@AndroidEntryPoint
 class NutritionFragment : BaseFragment() {
 
     private lateinit var binding: FragmentNutritionBinding
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,6 +26,24 @@ class NutritionFragment : BaseFragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentNutritionBinding.inflate(layoutInflater)
+
+        viewModelObserve()
+        viewModel.getDDay()
+
         return binding.root
+    }
+
+    @SuppressLint("SimpleDateFormat", "SetTextI18n")
+    private fun viewModelObserve() {
+        viewModel.dDayObserve.observe(viewLifecycleOwner) {
+            val datePref = it.substring(0..9)
+            val targetDate = SimpleDateFormat("yyyy.MM.dd").parse(datePref)!!.time
+            val today = Calendar.getInstance().time.time
+
+            val dDay = (today - targetDate) / (60*60*24*1000)
+
+            binding.tvDDay.text = "D$dDay"
+            binding.tvDate.text = it
+        }
     }
 }
