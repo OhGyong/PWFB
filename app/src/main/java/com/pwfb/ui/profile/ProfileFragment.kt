ced07b0aac0dac00d68321c63d9921bf4284b5c0
O_Gyong <ogyong7143@gmail.com>
2024-01-03 01:39:57 +0900

    1. 안드로이드 기본 배경 windowBackground 적용 2. 프로필 작업

--- app/src/main/java/com/pwfb/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/pwfb/ui/profile/ProfileFragment.kt
@@ -4,12 +4,30 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.prolificinteractive.materialcalendarview.CalendarDay
+import com.prolificinteractive.materialcalendarview.format.ArrayWeekDayFormatter
+import com.prolificinteractive.materialcalendarview.format.MonthArrayTitleFormatter
+import com.pwfb.R
 import com.pwfb.base.BaseFragment
 import com.pwfb.databinding.FragmentProfileBinding
+import com.pwfb.ui.MainViewModel
+import com.pwfb.util.ClearDecorator
+import com.pwfb.util.DayDisableDecorator
+import com.pwfb.util.SelectDecorator
+import com.pwfb.util.TodayDecorator
+import dagger.hilt.android.AndroidEntryPoint
+import java.time.LocalDate
 
+
+@AndroidEntryPoint
 class ProfileFragment : BaseFragment() {
 
     private lateinit var binding: FragmentProfileBinding
+    private val viewModel: MainViewModel by viewModels()
+
+    private var datePref = ""
+    private var timePref = ""
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,6 +35,71 @@ class ProfileFragment : BaseFragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentProfileBinding.inflate(layoutInflater)
+
+        setCalendarView()
+        viewModelObserve()
         return binding.root
+    }
+
+    private fun viewModelObserve() {
+
+    }
+
+    private fun setCalendarView() {
+        // 연, 월 헤더 스타일
+        binding.cvCalendar.setHeaderTextAppearance(R.style.CalendarWidgetHeader)
+
+        // 1~12월, 월간 표시
+        binding.cvCalendar.setTitleFormatter(MonthArrayTitleFormatter(resources.getTextArray(R.array.calendar_month)))
+
+        // 일~토, 주간 표시
+        binding.cvCalendar.setWeekDayFormatter(ArrayWeekDayFormatter(resources.getTextArray(R.array.calendar_week)))
+
+        // 헤더 년 월로 표시
+        binding.cvCalendar.setTitleFormatter { day ->
+            val inputText = day.date
+            val calendarHeaderElements = inputText.toString().split("-")
+            val calendarHeaderBuilder = StringBuilder()
+
+            calendarHeaderBuilder.append(calendarHeaderElements[0]).append("년 ")
+                .append(calendarHeaderElements[1]).append("월")
+
+            calendarHeaderBuilder.toString()
+        }
+
+        val today = CalendarDay.today()
+        val disabledDates = hashSetOf<CalendarDay>()
+
+        val dayDisableDecorator = DayDisableDecorator(disabledDates, today, requireContext().getColor(R.color.c_949292))
+        val todayDecorator = TodayDecorator(requireContext().getColor(R.color.c_caab3f))
+
+        binding.cvCalendar.addDecorators(dayDisableDecorator, todayDecorator)
+
+        // 날짜 선택 시 처리
+        binding.cvCalendar.setOnDateChangedListener { _, date, _ ->
+            binding.cvCalendar.addDecorators(
+                ClearDecorator(requireContext().getColor(R.color.white), date),
+                dayDisableDecorator, todayDecorator,
+                SelectDecorator(requireContext().getColor(R.color.c_caab3f), date)
+            )
+
+            val month = if(date.month<10) "0${date.month}" else "${date.month}"
+            val day = if(date.day<10) "0${date.day}" else "${date.day}"
+
+            datePref = "${date.year}.$month.$day${getWeek(date)}"
+        }
+    }
+
+    private fun getWeek(date: CalendarDay):String {
+        return when(LocalDate.of(date.year, date.month, date.day).dayOfWeek.value) {
+            1 -> "(월)"
+            2 -> "(화)"
+            3 -> "(수)"
+            4 -> "(목)"
+            5 -> "(금)"
+            6 -> "(토)"
+            7 -> "(일)"
+            else -> ""
+        }
     }
 }
