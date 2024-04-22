1c84d7fec7f9af161475d0b30e076c347c5f48cb
O_Gyong <ogyong7143@gmail.com>
2024-04-22 20:47:09 +0900

    versionCode 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.pwfb"
         minSdk = 29
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
